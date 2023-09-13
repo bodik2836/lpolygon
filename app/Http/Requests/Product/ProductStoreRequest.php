9bf9450c52b3670c684518303822bf8a50542c5e
Bohdan Tarasiuk <bodik2836@gmail.com>
2023-09-13 16:20:07 +0300

    Created ProductStoreRequest

---
+++ app/Http/Requests/Product/ProductStoreRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|decimal:1,2',
+            'image_url' => 'required|string|max:255',
+            'category_id' => 'required|exists:categories,id',
+        ];
+    }
+}
